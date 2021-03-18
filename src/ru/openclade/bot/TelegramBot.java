965eeec564b50ef42dd4e728ea8f9f860f06cf94
Valera <Valera@DESKTOP-6QNFKI4>
2021-03-19 00:24:41 +0300

    test

---
+++ src/ru/openclade/bot/TelegramBot.java
@@ -0,0 +1,69 @@
+package ru.openclade.bot;
+
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+
+
+public class TelegramBot extends TelegramLongPollingBot {
+	
+	public static void main(String[] args) {
+	ApiContextInitializer.init();
+	TelegramBotsApi telebot = new TelegramBotsApi();
+	try {
+		telebot.registerBot(new TelegramBot());
+	} catch(TelegramApiException e) {
+		e.printStackTrace();
+	}
+	}
+
+	@Override
+	public String getBotUsername() {
+		// TODO Auto-generated method stub
+		return "OpenClade";
+	}
+	public void sendMessage(Message message, String text, boolean isReply) {
+		SendMessage send = new SendMessage();
+		send.enableMarkdown(true);
+		send.setChatId(message.getChatId().toString());
+		if(isReply) {
+			send.setReplyToMessageId(message.getMessageId());
+		}
+		send.setText(text);
+		try {
+			sendMessage(send);
+		} catch(TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void onUpdateReceived(Update update) {
+		 
+		Message message = update.getMessage();
+		if(message != null && message.hasText()) {
+			switch(message.getText()) {
+			case "/help":
+				this.sendMessage(message, "Hello, Brother", true);
+				break;
+			case "/start":
+				this.sendMessage(message, "Стартуем!", false);
+				break;
+			default:
+				this.sendMessage(message, "Начни игру:  /start", false);
+		}
+			}
+			
+	}
+
+	@Override
+	public String getBotToken() {
+		// TODO Auto-generated method stub
+		return "1305724214:AAFwTq3eBExJXBxCHV7ZaIpP8hRPkkKCHts";
+	}
+}
